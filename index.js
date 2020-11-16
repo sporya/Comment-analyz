8a9d7cced711070563e8d766faeb98a375796ea1
s.porya pai <spp75spp@GMAIL.COM>
2020-11-16 10:56:27 +0330

    some change

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
-//import {Qs} from('https://cdnjs.cloudflare.com/ajax/libs/qs/6.7.0/qs.min.js') ;
-//import {axios} from('https://unpkg.com/axios/dist/axios.min.js') ;
+const Qs = require('./node_modules/qs');
+const axios= require('./node_modules/axios');
+
      // Text-Processing API Url
      const API_URL = 'https://japerk-text-processing.p.rapidapi.com/sentiment/';
      // RapidAPI request headers
@@ -9,62 +10,66 @@
        , 'X-RapidAPI-Key': '768d224b32mshbe5f76705cbfd9bp154850jsnba7a2201e140'
        , 'Content-Type': 'application/x-www-form-urlencoded'
      };
-     // Button click handler
-     export default function AnalyzeCommentText(Commenttxt) {
-       // Getting a textarea element with a comment
+
+
+
+     module.exports = function AnalyzeCommentText(Commenttxt) {
+    
        const commentElement = Commenttxt;
-       // Getting comment text
-      // const commentText = commentElement.value.trim();
-       // Handle empty comment
+     
        if (!commentElement) {
                return handleEmptyComment();
        }
-       // Calling the API and passing the result with the displayResult as a callback function
-       return analyzeComment(commentElement, displayResult);
+       if(commentElement){
+         return displayResult(commentElement);
+       }
+      
+   
+      
      };
-     const analyzeComment = (comment, callback) => {
-       // Creating an object to send to the server
-       const data = {
+     
+     function handleEmptyComment() {
+       return 'Your comment is empty';
+     };
+     function displayResult (comment) {
+       
+      const datacoment = {
                text: comment
                , language: 'english'
        };
-       // Encoding data for application/x-www-form-urlencoded content type
-       const formattedData = Qs.stringify(data);
-       // POST request to server
+      
+       const formattedData = Qs.stringify(datacoment);
+     
+       
        axios.post(API_URL, formattedData, { headers: REQUEST_HEADERS })
                .then(response => {
+                var resultText = '';
                  const data = response.data;
-                 // Calling a callback function with data from the server
-                 callback(data)
+                 switch (data.label) {
+                  case 'pos':
+                      resultText = 'Wow! Your comment is very positive!';
+                      break;
+                  case 'neg':
+                      resultText = 'Negative comment =(';
+                      break;
+                  case 'neutral':
+                      resultText = 'Simple comment =)';
+                      break;
+                  default:
+                      resultText = 'Hmmm, cant understand your comment';
+              }
+              const resultElement = resultText
+      
+              if (resultElement){
+                console.log(resultElement);
+                
+              }
+              if(!resultElement){
+                console.log('false');
+              }
+              return 'resultElement';
                })
                .catch(error => console.error(error))
-     };
-     function handleEmptyComment() {
-       return alert('Your comment is empty');
-     };
-     const displayResult = result => {
-      
-       // Setting the color of the result text depending on the response label
-       const label = result.label;
-       const resultElement = document.getElementById('result');
-       resultElement.setAttribute('class', label);
-       var resultText = '';
-       // Choosing the result text depending on response label
-       switch (label) {
-           case 'pos':
-               resultText = 'Wow! Your comment is very positive!';
-               break;
-           case 'neg':
-               resultText = 'Negative comment =(';
-               break;
-           case 'neutral':
-               resultText = 'Simple comment =)';
-               break;
-           default:
-               resultText = 'Hmmm, cant understand your comment';
-       }
-       // Setting the result text
-       resultElement.textContent = resultText;
-      return resultText;
+     
      };
       
